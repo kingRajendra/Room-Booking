f9c084d525d0c6886a3a681c6759f363f300b499
kingRajendra <rajendraroccss@gmail.com>
2022-10-16 10:28:52 +0530

    rooms added

--- src/Hotel.js
+++ src/Hotel.js
@@ -1,21 +1,15 @@
 import './App.css';
 import React from "react";
-import { useState, useEffect } from "react";
+import { useState,useEffect } from "react";
 
-export default function Hotel() {
 
-    const [adults, setAdults] = useState(2);
-    const [children, setChildren] = useState(0);
-    const [rooms, setRooms] = useState(1);
+const Room = () => {
+
     const [state, setState] = useState({});
-
+    const [adults, setAdults] = useState(1);
+    const [children, setChildren] = useState(0);
     const [childrenValues, setChildrenValues] = useState([]);
     const [childrenArr, setChildrenArr] = useState([]);
-
-    const [roomValues, setRoomValues] = useState([]);
-    const [roomArr, setRoomArr] = useState([]);
-
-    const [setGuestNumber, setRoomNumber] = useState([]);
 
 
     //Adult Number
@@ -47,33 +41,6 @@ export default function Hotel() {
         setChildrenArr(new Array(children - 1).fill(0));
     };
 
-    //Rooms Number
-
-    const handleRoomsChange = (e) => {
-        setRooms(e.target.value);
-        setRoomArr(new Array(rooms + 1).fill(0));
-    };
-    const handleRoomsAdd = () => {
-        setRooms(Number(rooms) + 1);
-        setRoomArr(new Array(rooms + 1).fill(0));
-    };
-    const handleRoomsReduce = () => {
-        setRooms(Number(rooms) - 1);
-        setRoomArr(new Array(rooms - 1).fill(0));
-    };
-
-    //Reset 
-
-    const handleReset = () => {
-        setAdults(1);
-        setRooms(1);
-        setChildren(0);
-
-
-        setChildrenArr(new Array(0).fill(0));
-        setRoomArr(new Array(0).fill(0));
-    };
-
     //Children Age
 
     const handleChildrenAge = (event) => {
@@ -89,17 +56,6 @@ export default function Hotel() {
         console.log(state);
     }, [state]);
 
-    // Rooms Number
-
-    const handleRooms = (event) => {
-        const name = event.target.name;
-        setState({
-            ...state,
-            [name]: event.target.value,
-        });
-        console.log(state);
-        setRoomValues([...roomValues, state]);
-    };
 
     useEffect(() => {
         console.log(state);
@@ -107,39 +63,10 @@ export default function Hotel() {
 
     console.log(childrenValues)
     console.log(childrenArr)
-    console.log(roomArr)
-
-    const handleApply = () => {
-        setGuestNumber(children + adults);
-        setRoomNumber(rooms);
-    };
 
     return (
-        <div className="main">
-            <h1 className='heading'>Hotel Reservation</h1>
-            <div className="card">
-                <h1 className="names">Rooms <span className='span'>{rooms}</span> & Guests <span>{children + adults}</span></h1>
-            </div>
-            <div className='room-card'>
-                <h1 className='naming'>Rooms {rooms}</h1>
-                <div className="row">
-                    <div>
-                        <button className="button" disabled={rooms === 0} onClick={handleRoomsReduce}>Remove</button>
-                    </div>
-
-                    <div>
-                        <button className="button" disabled={rooms === 10} onClick={handleRoomsAdd}>Add </button>
-                    </div>
-                </div>
-            </div>
-
-            <div room={rooms}>
-                {roomArr.map((el, index) => {
-                    console.log(index);
-                    return (
-
                         <div className='room-card'>
-                            <h1>Room: {index + 1}</h1>
+                            <h1>Room</h1>
                             <div className="row">
                                 <div className='col'>
                                     <h3 className='head'>Adults</h3>
@@ -153,13 +80,7 @@ export default function Hotel() {
                                         <div>
                                             <button className="button1" disabled={adults === 12} onClick={handleAdultsAdd}>+</button>
                                         </div>
-                                        <div>
-                                            {adults >= 10 && (
-                                                <div adults={adults}>
-                                                    <p className='text1'>Maximum Members</p>
-                                                </div>
-                                            )}
-                                        </div>
+                                    
                                     </div>
                                     <h3 className='head'>Children</h3>
                                     <div className="row">
@@ -203,18 +124,28 @@ export default function Hotel() {
                                 </div>
                             </div>
                         </div>
-                    )
-                })}
-                <div>
-                    <button onClick={handleReset} className="reset">
-                        Reset
-                    </button>
-                </div>
-                <div>
-                    <button onClick={handleApply} className="apply">
-                        Apply
-                    </button>
-                    <h1 className='total'>Rooms  <span>{rooms} </span>&  Guests <span>{adults + children}</span></h1>
+        
+                           
+    )
+}
+
+export default function Hotel() {
+
+    const [rooms, setRooms] = useState([])
+
+    return (
+        <div className="main">
+            <h1 className='heading'>Hotel Reservation</h1>
+            <div className="card">
+                <h1 className="names">Rooms</h1>
+            </div>
+            <Room/>
+             {rooms}
+            <div className='room-card'>
+                <div className="row">
+                    <div>
+                        <button className="button" onClick={() => setRooms([...rooms, <Room/>])}>Add Room</button>
+                    </div>
                 </div>
             </div>
         </div>
